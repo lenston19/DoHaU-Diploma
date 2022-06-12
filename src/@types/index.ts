8e5af0730de764114103d1fc1184e91ed9f4d6dd
lenston19 <lenston190900@gmail.com>
2022-06-12 22:22:39 +0300

    refactoring component with links

---
+++ src/@types/index.ts
@@ -0,0 +1,4 @@
+export interface navigationLinks {
+  url: string;
+  name: string;
+}
---
+++ src/components/EssentialLinks.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-list>
+    <q-item v-for="link in links" :key="link.name" :to="link.url" active-class="bg-accent" clickable>
+      <q-item-section class="text-h6">{{
+          link.name
+      }}</q-item-section>
+      <q-item-section avatar>
+        <q-icon color="white" name="las la-arrow-left" />
+      </q-item-section>
+    </q-item>
+  </q-list>
+  <q-separator />
+</template>
+
+<script setup lang="ts">
+import { navigationLinks } from 'types/index'
+
+defineProps<{
+  links: navigationLinks[]
+}>()
+
+</script>
